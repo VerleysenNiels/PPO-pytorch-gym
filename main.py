99629d63fc89a1f90ad5de54c73485cdd66c0b5e
Niels Verleysen <nlsverleysen99@gmail.com>
2023-03-10 15:33:35 +0100

    Init with standard seeding and setting cuda

---
+++ main.py
@@ -0,0 +1,20 @@
+# Imports
+import random
+import gym
+import numpy as np
+import torch
+
+# Arguments (TODO: handle with argument parser)
+SEED = 1
+DETERMINISTIC_TORCH = True
+USE_GPU = True
+
+if __name__ == "__main__"
+# SEEDING
+random.seed(SEED)
+np.random.seed(SEED)
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = DETERMINISTIC_TORCH
+
+# Use GPU
+device = torch.device("cuda" if torch.cuda.is_available() and USE_GPU else "cpu")
